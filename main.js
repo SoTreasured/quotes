df93d712d8f4f9053d527e924f2ba6093337eb97
Treasure <treasureihedigbo1@gmail.com>
2020-09-24 09:08:22 +0100

    finally did the setInterval thingy

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-var randomIndex = Math.floor(Math.random() * 3);
-var show;
-
 var quote = [
-"\"The great thing about quotes is that they pack so much wisdom in so few words \"",
-"\"Quotations have made a positive difference in my Life, in some they have changed the way I think about a situation \"",
-"\" I often quote myself.It adds spice to my conversation\""
+"\"The great thing about inspiring quotes is that they pack so much wisdom in so few words.\"<br> <i> -Chris Hughes</i>",
+"\"Quotations have made a positive difference in my Life, in some they have changed the way I think about a situation \"<br> <i> -Catherine Pulsifer</i>",
+"\" I often quote myself.It adds spice to my conversation.\"<br> <i> -George BernardShaw</i>",
+"\" The beauty of quotes is that they allow us to glimpse into another minds and understand how they think and look at the world.\"<br> <i> -Joseph Geran II</i>",
+"\"For many of us, inspiration is triggered by the quips and quotations of others.\"<br> <i> -Emilee Day</i> ",
+"\" A quote is a powerful tool for our mind, our soul, our spirit. A quote can sharpen your mind and give you and instant and rapid relief if your soul is down, in a dark time. \"<br> <i> -Dotchamou Zakari</i>",
+"\"An apt quotation is like a lamp which flings its light over the whole sentence \"<br> <i> -Letitia Landon</i>"
 ];
 
 function showQuote(){
-    titleQuote = document.getElementById("title")
-    titleQuote.textContent = quote[randomIndex];
+    const randomIndex = Math.floor(Math.random() * quote.length);
+    const titleQuote = document.getElementById("title")
+    titleQuote.innerHTML =  quote[randomIndex];
 }
 
-function interval(){
-    show = setInterval(showQuote, 3000);
-}
 
-showQuote();
+   setInterval(showQuote, 3000);
+
+
+
